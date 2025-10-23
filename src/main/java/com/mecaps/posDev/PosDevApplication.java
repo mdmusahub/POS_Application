fa1a95af40c747875071d3352f8ab0a641aba90f
MD MUSA ALAM <157529747+mdmusahub@users.noreply.github.com>
2025-10-23 12:04:02 +0530

    first commit

---
+++ src/main/java/com/mecaps/posDev/PosDevApplication.java
@@ -0,0 +1,13 @@
+package com.mecaps.posDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mecaps/posDev/PosDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mecaps.posDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
