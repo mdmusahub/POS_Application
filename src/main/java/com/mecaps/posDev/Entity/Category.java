062ddb63b198cb848e25099474f0ef83dfa2f971
mohdsahil798 <mohdsah276@gmail.com>
2025-10-27 10:48:01 +0530

    create 4 entity

---
+++ src/main/java/com/mecaps/posDev/Entity/Category.java
@@ -0,0 +1,26 @@
+package com.mecaps.posDev.Entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long category_id;
+    private String category_name;
+    private String category_description;
+
+    @ManyToOne
+    private Category parent_category;
+
+    @OneToMany(mappedBy = "parent_category", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Category> subCategories = new ArrayList<>();
+
+    @OneToMany(mappedBy = "category_id", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Product> products = new ArrayList<>();
+}
