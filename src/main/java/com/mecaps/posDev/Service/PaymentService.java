5e9042fcb822d788f45d179b656810e7bab2abf5
mohdZohaib-web <mohdjoyeb786@gmail.com>
2025-12-03 10:15:59 +0530

    solving error for pull

--- src/main/java/com/mecaps/posDev/Service/PaymentService.java
+++
@@ -1,8 +0,0 @@
-package com.mecaps.posDev.Service;
-
-import org.json.JSONObject;
-
-public interface PaymentService {
-    JSONObject createOrder(int amount) throws Exception;
-    boolean verifyPayment(String orderId, String paymentId, String signature) throws Exception;
-}
